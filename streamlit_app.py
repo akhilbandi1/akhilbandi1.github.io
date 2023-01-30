d175705d99cd13f5fb0cf8f2bbdfeba3def9c3b3
akhilbandi1 <42867015+akhilbandi1@users.noreply.github.com>
2023-01-30 17:08:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
 streamlit.text('Omega 3 and burberry oat meal')
 streamlit.text('Hard Boiled 3 range egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
