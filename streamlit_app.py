e89d812f857b501581e6b5f639543556c12af4df
akhilbandi1 <42867015+akhilbandi1@users.noreply.github.com>
2023-01-30 17:06:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 streamlit.header ('Breakfast Menu')
 streamlit.text('Omega 3 and burberry oat meal')
+streamlit.text('Hard Boiled 3 range egg')
